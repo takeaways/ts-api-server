5c2411f680bc89ad143d8ac8b4f9e9b19510b314
JangGeonil <>
2020-05-05 17:20:14 +0900

    pro

--- app.ts
+++ app.ts
@@ -53,7 +53,7 @@ app.use(
 		secret: process.env.COOKIE_SECRET!,
 		cookie: {
 			httpOnly: true,
-			secure: true,
+			secure: false,
 			domain: ENV ? '.geoniljang.com' : undefined,
 		},
 		name: 'ngi',
