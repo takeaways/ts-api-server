e0cdd993aa7720275c6cdbf65ff5175bfe170c75
JangGeonil <>
2020-05-05 18:00:50 +0900

    check cookie login

--- middlewares/loginCheck.ts
+++ middlewares/loginCheck.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
-	console.log('------>', req.cookies);
+	console.log('------>', req.headers.cookie);
 
 	if (req.isAuthenticated()) {
 		next();
